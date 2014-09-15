a36021a420f94a8adf71c1fdcdb72fc6278bd3ad
j9recurses <j9.heiser@ischool.berkeley.edu>
2014-09-15 04:49:48 -0700

    more

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140915072324) do
+ActiveRecord::Schema.define(version: 20140915103243) do
 
   create_table "access_codes", force: true do |t|
     t.string   "user_access_code"
@@ -46,6 +46,91 @@ ActiveRecord::Schema.define(version: 20140915072324) do
     t.text     "description"
     t.string   "model_name"
     t.string   "cost_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "election_profile_descriptions", force: true do |t|
+    t.string   "field"
+    t.string   "label"
+    t.string   "model_name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "election_profiles", force: true do |t|
+    t.string   "epems"
+    t.boolean  "eppphwscan"
+    t.boolean  "eppphwdre"
+    t.boolean  "eppphwmarkd"
+    t.boolean  "eppphwpollbk"
+    t.boolean  "eppphwoth"
+    t.string   "epetallysys"
+    t.integer  "epppbalpap"
+    t.integer  "epppbalaccsd"
+    t.integer  "eprv"
+    t.integer  "eppploc"
+    t.integer  "epprecwpp"
+    t.integer  "epprecvbm"
+    t.integer  "epbaltype"
+    t.integer  "epbalpage"
+    t.integer  "epbalsampvip"
+    t.integer  "epvipinsrt"
+    t.integer  "epbalofficl"
+    t.integer  "epvbmmail"
+    t.integer  "epvbmmailprm"
+    t.integer  "epvbmmailmbp"
+    t.integer  "epvbmmailuo"
+    t.integer  "epvbmotc"
+    t.integer  "epvbmret"
+    t.integer  "epvbmretprm"
+    t.integer  "epvbmretmbp"
+    t.integer  "epvbmretuo"
+    t.integer  "epvbmundel"
+    t.integer  "epvbmchal"
+    t.integer  "epvbmprovc"
+    t.integer  "epvbmprovnc"
+    t.integer  "eplangnoeng"
+    t.integer  "epcand"
+    t.integer  "epcandfsc"
+    t.integer  "epcandcd"
+    t.integer  "epcandwi"
+    t.integer  "epcandwifsc"
+    t.integer  "epcandwicd"
+    t.integer  "epmeasr"
+    t.integer  "epmeasrfsc"
+    t.integer  "epmeasrcd"
+    t.float    "epicrp"
+    t.boolean  "epicrpfed"
+    t.boolean  "epicrpcounty"
+    t.boolean  "epicrpown"
+    t.boolean  "epicrpoth"
+    t.integer  "eptotindirc"
+    t.integer  "eptotelectc"
+    t.boolean  "epcostallrg"
+    t.boolean  "epcostallpre"
+    t.boolean  "epcostallopp"
+    t.boolean  "epcostalloth"
+    t.integer  "eptotbilled"
+    t.integer  "eptotcounty"
+    t.integer  "eptotsb90c"
+    t.integer  "eptotsb90r"
+    t.text     "epmandates"
+    t.boolean  "started",                  default: false
+    t.boolean  "complete",                 default: false
+    t.string   "current_step"
+    t.integer  "county"
+    t.integer  "election_year_profile_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "election_year_profiles", force: true do |t|
+    t.string   "year"
+    t.date     "election_dt"
+    t.integer  "year_dt"
+    t.string   "election_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@ fieldstodelete = ['salbaltotbe','salbaltotbep', 'salbaltothrs','salpptotbe','sal
 fieldstodelete.each do | fd|
   CategoryDescription.where(:field => fd).destroy_all
 end
+
+#electionprofilelist = [[ '2014 Primary Election' , '06-03-2014', 2014, 'Primary'],['2012 General Election', '11-06-2012', 2012, 'General'],['2012 Presidential Primary Election', '06-05-2012', 2012 , 'Presidential Primary'],['2010 General Election' , '11-02-2010, 2010, 'General' ],['2010 Primary Election',  '06-08-2010', 2010, 'Primary'],['2009 Special Election', '05-19-2009', 2009, 'Special'],['2008 General Election' , '11-04-2008', 2008, 'General' ],['2008 Primary Election', '06-03-2008', 2008, 'Primary'],['2008 Presidential Primary Election' '02-05-2008', 2008, 'Presidential Primary'],['2006 General Election', '11-07-2006', 2006, 'General'],['2006 Primary Election' '06-06-2006', 2006, 'Primary'],['2005 Special Election' '11-08-2005', 2005, 'Special'],['2004 General Election', '11-02-2004', 2004, 'General'],['2004 Primary Election', '03-02-2004', 2004, 'Primary'] ]
+
+#'election_profiles_descrip.csv'
