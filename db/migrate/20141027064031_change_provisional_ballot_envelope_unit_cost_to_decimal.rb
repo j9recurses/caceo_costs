1fc8a327810a61fd38fd5bd077425506924b6252
sethdn <seth.neill@gmail.com>
2014-10-27 03:23:18 -0700

    implemented, not migrated in case of data loss

--- db/migrate/20141027064031_change_provisional_ballot_envelope_unit_cost_to_decimal.rb
+++
@@ -1,5 +0,0 @@
-class ChangeProvisionalBallotEnvelopeUnitCostToDecimal < ActiveRecord::Migration
-  def change
-    change_column :ssbals, :ssbalpriprou, :decimal, precision: 3, scale: 2
-  end
-end
