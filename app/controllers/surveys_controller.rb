63a8c92be8e255ffb8db949f3cfb165c2ab8c766
sethdn <seth.neill@gmail.com>
2014-11-21 14:53:31 -0800

    finish controller and show refactor, still only on salbals

---
+++ app/controllers/surveys_controller.rb
@@ -0,0 +1,92 @@
+class SurveysController < ApplicationController
+  before_action :authenticate_user
+  before_action :get_user, :get_year, :get_category
+  before_action :model_singular
+  before_action :object, only: [:show, :update, :edit, :destroy]
+  before_action :load_wizard, only: [:new, :edit, :create, :update]
+  before_action :make_survey_form_model,  except: :destroy
+
+  def index
+   if @category.started
+      id = klass.where(county: @user[:county], election_year_id: @election_year_id).pluck(:id).last
+      redirect_to send("#{model_singular}_path", id)
+    else
+      redirect_to send("new_#{model_singular}_path")
+    end
+  end
+
+  def show
+    @survey_data_model = object
+    render file: "#{ Rails.root.join('app/views/surveys/show') }"
+  end
+
+  def new
+    render file: "#{ Rails.root.join('app/views/surveys/new') }"
+  end
+
+  def edit
+    render file: "#{ Rails.root.join('app/views/surveys/edit') }"
+  end
+
+  def create
+    @year_element = @election_year.year_elements.create(:element => @survey_data_model)
+    if @wizard.save && @year_element.save
+      klass.category_status(@category.id, @survey_data_model)
+      redirect_to @survey_data_model, notice: "The #{@category.name} Costs That You Entered For #{@election_year[:year]} were Successfully Saved."
+    else
+      render file: "#{ Rails.root.join('app/views/surveys/new') }"
+    end
+  end
+
+
+  def update
+    if @wizard.save
+      klass.category_status( @category.id, @survey_data_model)
+      redirect_to @survey_data_model, notice: "The #{@category.name} Costs That You Entered For #{@election_year[:year]} were Successfully Updated."
+    else
+      render file: "#{ Rails.root.join('app/views/surveys/edit') }"
+    end
+  end
+
+  def destroy
+    klass.remove_category_status(@category.id)
+    object.destroy
+    redirect_to salbals_path
+  end
+
+  private
+
+  def make_survey_form_model
+    @survey_form_model ||= CategoryDescription.where(model_name: model_name)
+  end
+
+  def model_name
+    @model_name ||= self.class.to_s.gsub('Controller', '').downcase
+  end
+
+  def model_singular
+    @model_singular ||= model_name.singularize
+  end
+
+  def klass
+    @klass ||= model_name.singularize.capitalize.constantize
+  end
+
+  def object
+    @object ||= klass.find(params[:id])
+  end
+
+  def load_wizard
+    @wizard ||= ModelWizard.new( @object || klass, session, params)
+    if self.action_name.in? %w[new edit]
+      @wizard.start
+    elsif self.action_name.in? %w[create update]
+      @wizard.process
+    end
+    @survey_data_model = @wizard.object
+  end
+
+  def get_category
+    @category= Category.find_by(election_year_id: @election_year_id, county: @user[:county],  model_name: model_name)
+  end
+end
--- app/presenters/survey_presenter.rb
+++ app/presenters/survey_presenter.rb
@@ -9,30 +9,76 @@ class SurveyPresenter
 
   def_delegators :@survey_data, :current_step, :total_steps, :first_step?, :last_step?
 
+  SURVEY_SHOW_SECTIONS = %w{ salary_estimate benefits_percent benefits_dollar hours comment salary service_supply }
+
+  SURVEY_SHOW_SECTIONS.each do |name|
+    define_method("#{name}_items") do
+      show_item_lists[name] ||= {}
+    end
+
+    define_method("#{name}_total") do
+      self.send("#{name}_items").values.compact.inject { |sum, item_value| sum + item_value }
+    end
+  end
+
+  def klass
+    @survey_data.class
+  end
+
+  def show_item_lists
+    @survey_show_item_lists ||= {}
+  end
+
   def title_show
     "#{survey_name.gsub('Salaries Related To', '')} Salaries Summary for the"
   end
 
-  def benefit_percent_items
-    @benefit_percent_items ||= @survey_form.find_all { |form_item|
-      form_item.label =~ /Benefits.*percent/
-    }.map do |form_item|
-
-      item_label = form_item.label
-      item_value = @survey_data[ item_label ]
-      { item_label => item_label.nil? ? 0 : item_label }
+  def sort_form_items
+    if cost_type == 'salaries'
+      sort_salaries
+    elsif cost_type == 'services & supplies'
+      sort_services_supplies
     end
   end
 
-  def benefit_percent_total
-    benefit_percent_items.inject { |sum, item| sum + item. }
+  def sort_salaries
+    @survey_form.each do |item|
+      item_value = @survey_data[ item.field ]
+      case item.label
+      when /Salaries/
+        salary_estimate_items[item.label] = item_value.nil? ? 0 : item_value
+      when /Benefits.*percent/
+        benefits_percent_items[item.label] = item_value.nil? ? 0 : item_value
+      when /Benefits.*dollars/
+        benefits_dollar_items[item.label] = item_value.nil? ? 0 : item_value
+      when /Hours/
+        hours_items[item.label] = item_value.nil? ? 0 : item_value
+      when /General/
+        comment_items[item.label] = item_value
+      else
+        salary_items[item.label] = item_value.nil? ? 0 : item_value
+      end
+    end
   end
 
-  def benefit_dollar_items
-
+  def sort_services_supplies
+    @survey_form.each do |item|
+      item_value = @survey_data[ item.field ]
+      if FilterCost.find_by(filtertype: "comment").fieldlist.include?(item.field)
+        comment_items[item.label] = item_value
+      else
+        service_supply_items[item.label] = item_value.nil? ? 0 : item_value
+      end
+    end
   end
 
+  def salary_estimate_benefits_total
+    benefits_dollar_total + salary_estimate_total
+  end
 
+  def salary_benefits_total
+    benefits_dollar_total + salary_total
+  end
 
   def data
     @survey_data
@@ -51,7 +97,7 @@ class SurveyPresenter
   end
 
   def cost_type
-    @cost_type ||= @survey_form.pluck(:cost_type).uniq
+    @survey_form.pluck(:cost_type).uniq.first
   end
 
   def header_text
