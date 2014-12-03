fc29b1a4e5f67f1f9cf041758d8ddbb36fa5a95b
sethdn <seth.neill@gmail.com>
2014-12-02 17:59:26 -0800

    current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,4 +37,8 @@ class ApplicationController < ActionController::Base
     end
   end
 
+private
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
