b9d37acb2e179f1c5a58f8a3f24dc33d93632ae6
sethdn <seth.neill@gmail.com>
2014-12-05 17:19:42 -0800

    fix sql bug, sort by updated-at

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,13 +1,13 @@
 class ActivitiesController < ApplicationController
   def index
     if params[:day]
-      @activities = Activity.sort_by_county_election Activity.activity_since( Time.now.beginning_of_day )
+      @activities = Activity.sort_by_county_election Activity.since( Time.now.beginning_of_day )
       @title = "Today's Activity"
     elsif params[:week]
-      @activities = Activity.sort_by_county_election Activity.activity_since( Time.now.beginning_of_week )
+      @activities = Activity.sort_by_county_election Activity.since( Time.now.beginning_of_week )
       @title = "This Week's Activity"
     elsif params[:all]
-      @activities = Activity.sort_by_county_election Activity.all_activity
+      @activities = Activity.sort_by_county_election Activity.all
       @title = "All open surveys"
     end
   end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,26 +1,28 @@
 class Activity
   SURVEYS = [Postage, Salbal, Salbc, Salcan, Saldojo, Salmed, Saloth, Salpp, Salpw, Salvbm, Ssbal, Ssbc, Sscan, Ssmed, Ssoth, Sspp, Sspw, Ssveh]
 
-  def self.activity_since(time)
+  def self.since(time)
     result = []
     SURVEYS.each do |klass|
       model_name = klass.to_s.downcase.pluralize
-      result.concat klass.select("MAX(#{model_name}.id), '#{model_name}' AS model_name, county, #{model_name}.updated_at, election_years.year AS election_name, election_years.election_dt AS election_date, category_descriptions.name AS survey_name")
+      result.concat klass.select("MAX(#{model_name}.id) AS id, '#{model_name}' AS model_name, county, #{model_name}.updated_at, election_years.year AS election_name, election_years.election_dt AS election_date, category_descriptions.name AS survey_name")
+        .where.not(county: 59)
         .where("#{model_name}.updated_at > ?", time)
         .group(:county, :election_year_id)
-        .joins("INNER JOIN election_years ON election_years.id = #{model_name}.id")
+        .joins("INNER JOIN election_years ON election_years.id = #{model_name}.election_year_id")
         .joins("INNER JOIN category_descriptions ON category_descriptions.model_name = '#{model_name}'")
     end
     result
   end
 
-  def self.all_activity
+  def self.all
     result = []
     SURVEYS.each do |klass|
       model_name = klass.to_s.downcase.pluralize
-      result.concat klass.select("MAX(#{model_name}.id), '#{model_name}' AS model_name, county, #{model_name}.updated_at, election_years.year AS election_name, election_years.election_dt AS election_date, category_descriptions.name AS survey_name")
+      result.concat klass.select("MAX(#{model_name}.id) AS id, '#{model_name}' AS model_name, county, #{model_name}.updated_at, election_years.year AS election_name, election_years.election_dt AS election_date, category_descriptions.name AS survey_name")
+        .where.not(county: 59)
         .group(:county, :election_year_id)
-        .joins("INNER JOIN election_years ON election_years.id = #{model_name}.id")
+        .joins("INNER JOIN election_years ON election_years.id = #{model_name}.election_year_id")
         .joins("INNER JOIN category_descriptions ON category_descriptions.model_name = '#{model_name}'")
     end
     result
@@ -34,7 +36,7 @@ class Activity
       sorted_surveys = surveys.sort {|x,y| y.election_date <=> x.election_date } 
       survey_hash = sorted_surveys.group_by { |x| x.election_name }
       survey_hash.each do |elec, survs|
-        survey_hash[elec] = survs.sort {|x,y| x.survey_name <=> y.survey_name }
+        survey_hash[elec] = survs.sort {|x,y| y.updated_at <=> x.updated_at }
       end
 
       { county_name: CaCountyInfo.find(county_id).name,
