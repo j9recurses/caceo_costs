708ce44b23929311af391dc246e5ba2b8d92ecb9
sethdn <seth.neill@gmail.com>
2015-01-23 16:06:13 -0800

    move max int input to 500,000,000

--- app/models/ssbal.rb
+++ app/models/ssbal.rb
@@ -16,7 +16,7 @@ class Ssbal < ActiveRecord::Base
     :ssbalpriuo, :ssbalpriprot, :ssbalpriship, :ssbalprioth, 
     numericality: { only_integer: true, 
       :greater_than_or_equal_to => 0, 
-      :less_than_or_equal_to  => 30000000, 
+      :less_than_or_equal_to  => 500000000, 
       :allow_nil => true, 
       :allow_blank => false, 
       message: " Entry is not valid. Please check your entry" }
