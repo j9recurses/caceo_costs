6994b69caf07253b360a9c5c4b3b60ef1ce4b9a4
sethdn <seth.neill@gmail.com>
2015-01-30 13:11:00 -0800

    flash messages on election_year and election_year_profile controllers toannounce 2014 gen ed is unavailable

--- app/controllers/election_year_profiles_controller.rb
+++ app/controllers/election_year_profiles_controller.rb
@@ -1,5 +1,7 @@
 class ElectionYearProfilesController < ApplicationController
   def election_profile_home
     @election_years = ElectionYearProfile.all
+    flash.now[:notice] = "Surveys for the 2014 General Election are temporarily unavailable, please check back later today to find them. Thanks!"
+
   end
 end
--- app/controllers/election_years_controller.rb
+++ app/controllers/election_years_controller.rb
@@ -1,5 +1,6 @@
 class ElectionYearsController < ApplicationController
   def home
     @election_years = ElectionYear.all
+    flash.now[:notice] = "Surveys for the 2014 General Election are temporarily unavailable, please check back later today to find them. Thanks!"
   end
 end
