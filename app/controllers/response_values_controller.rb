7eb837673093fd8108845798a2193eee5397c7d4
sethdn <seth.neill@gmail.com>
2015-07-30 10:56:31 -0700

    tiny serialization thing

--- app/controllers/response_values_controller.rb
+++ app/controllers/response_values_controller.rb
@@ -2,7 +2,7 @@ class ResponseValuesController < ActionController::Base
   def index
     respond_to do |format|
       format.json do
-        render json: ResponseValue.joins(:survey_response).where('NOT(survey_responses.county_id = 59)').count
+        render json: ResponseValue.joins(:survey_response).where('NOT(survey_responses.county_id = 59)')
       end
     end  
   end
